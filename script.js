bfd506424b6cdaee5d90f938174a53270bf965df
k.c.wong.3 <119720871+khad3@users.noreply.github.com>
2026-02-15 21:10:01 +0800

    more

--- script.js
+++ script.js
@@ -37,6 +37,18 @@ const PHOTOS = [
     'photo4.jpg',
     'photo5.jpg',
     'photo6.jpg',
+        'photo1.jpg',
+    'photo2.jpg',
+    'photo3.jpg',
+    'photo4.jpg',
+    'photo5.jpg',
+    'photo6.jpg',
+    'photo1.jpg',
+    'photo2.jpg',
+    'photo3.jpg',
+    'photo4.jpg',
+    'photo5.jpg',
+    'photo6.jpg'
 ];
 
 // ========================================
